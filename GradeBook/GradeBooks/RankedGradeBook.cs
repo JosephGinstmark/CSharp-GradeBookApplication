6901b3c8b05495c44cf21b6653162e33fe4e5047
JosephGinstmark <40714739+JosephGinstmark@users.noreply.github.com>
2019-06-08 14:52:43 +0200

    completed

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -31,7 +31,8 @@ namespace GradeBook.GradeBooks
        {
             if(Students.Count <5)
             {
-            Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
             }
             else base.CalculateStatistics();
 
